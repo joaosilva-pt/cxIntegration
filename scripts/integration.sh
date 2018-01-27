ccb959fc736b543c45661be9b94320d6c0fece92
joaosilva-pt <m.joao.silva.ml@gmail.com>
2018-01-27 15:56:58 +0000

    Scripts: all configuration options set on the begining of the script file

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+# =================
+# BEGIN OF CONFIG
+#
+# Edit the vars in this area according to project's needs
+#==================
 readonly EMAIL_RECIPIENTS="recipient1@gmail.com,recipient2@example.com"
-readonly GIT_REPO_URL="https://github.com/boostorg/boost.git"
-readonly STAGING_BASE="$HOME/checkmarx/staging"
-readonly REPO_SHORT_NAME="bricks"
-readonly LOCAL_REPO="$STAGING_BASE/$REPO_SHORT_NAME"
+readonly GIT_REPO_URL="https://github.com/joaosilva-pt/bricks"
+readonly CX_CONSOLE="$HOME/bin/CxConsolePlugin-8.51.0-20171024-0825/runCxConsole.sh"
+# =================
+# END OF CONFIG
+#
+# DO NOT EDIT BELLOW THIS POINT!!!!
+#==================
 
-function pullCode()
-{
-	if [[ ! -d "$STAGING_BASE" ]]; then
-		# Staging path does not exist. Create it
-		mkdir -p %STAGING_BASE
-	fi
-
-	# Clones repo
-	git clone "$GIT_REPO_URL" "$LOCAL_REPO"
-}
 
 function scanSourceCode()
 {
-	local readonly CX_CONSOLE=$HOME/checkmarx/CxConsolePlugin-8.51.0-20171024-0825/runCxConsole.sh
 
 	echo "$SCAN_RESULT"
 }
@@ -35,7 +32,6 @@ function sendSummaryEmail()
 	echo "$MAIL_CONTENT" | mail -s "CI summary report" $EMAIL_RECIPIENTS
 }
 
-pullCode
 XML_REPORT=scanSourceCode
 analyzeReport $XML_REPORT
 sendSummaryEmail
