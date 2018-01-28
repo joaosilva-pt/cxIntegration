410ddd5e03211864fa1427456db704c4e7bc052e
joaosilva-pt <m.joao.silva.ml@gmail.com>
2018-01-28 13:12:38 +0000

    Updates hookup script

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -8,6 +8,15 @@
 readonly EMAIL_RECIPIENTS="recipient1@gmail.com,recipient2@example.com"
 readonly GIT_REPO_URL="https://github.com/joaosilva-pt/bricks"
 readonly CX_CONSOLE="$HOME/bin/CxConsolePlugin-8.51.0-20171024-0825/runCxConsole.sh"
+
+readonly CX_SERVER="https://cxprivatecloud.checkmarx.net/"
+readonly CX_USER="m.joao.silva.ml@gmail.com"
+readonly CX_PASSWORD="0x12heckM@rx"
+readonly CX_LOCATION_TYPE="GIT"
+readonly CX_LOCATION_URL="https://github.com/joaosilva-pt/bricks"
+readonly CX_LOCATION_BRANCH="refs/head/master"
+readonly CX_PROJECT="CxServer\\SP\\EMEA\\gmail.com-56\\Bricks"
+readonly CX_XML_REPORT="/home/jsilva/develop/cxIntegration/scripts/report.xml"
 # =================
 # END OF CONFIG
 #
@@ -17,8 +26,7 @@ readonly CX_CONSOLE="$HOME/bin/CxConsolePlugin-8.51.0-20171024-0825/runCxConsole
 
 function scanSourceCode()
 {
-
-	echo "$SCAN_RESULT"
+	`"$CX_CONSOLE" scan -CxServer "$CX_SERVER" -CxUSer "$CX_USER" -CxPassword "CX_PASSWORD" -LocationType "$CX_LOCATION_TYPE" -LocationURL "$CX_LOCATION_URL" -LocationBranch "$CX_LOCATION_BRANCH" -ProjectName "$CX_PROJECT" -ReportXML "CX_XML_REPORT" -v`
 }
 
 function analyzeReport()
@@ -32,6 +40,6 @@ function sendSummaryEmail()
 	echo "$MAIL_CONTENT" | mail -s "CI summary report" $EMAIL_RECIPIENTS
 }
 
-XML_REPORT=scanSourceCode
-analyzeReport $XML_REPORT
+scanSourceCode
+analyzeReport "$CX_XML_REPORT"
 sendSummaryEmail
