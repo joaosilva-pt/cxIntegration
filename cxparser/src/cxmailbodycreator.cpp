2ba3db97c351b9dbe062cb9f85ccf53723c8f370
joaosilva-pt <m.joao.silva.ml@gmail.com>
2018-01-30 15:24:58 +0000

    Adds class files for mail's body creation

---
+++ cxparser/src/cxmailbodycreator.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file cxmailbodycreator.cpp
+ * @brief Implementation file for class cxMailBodyCreator
+ *
+ *  @author		Joao Silva <m.joao.silva.ml at gmail.com>
+ *  @date		Jan 30, 2018
+ */
+
+
+#include "cxmailbodycreator.h"
+
+cxIntegration::cxMailBodyCreator::cxMailBodyCreator() { }
+
+cxIntegration::cxMailBodyCreator::cxMailBodyCreator(const cxIntegration::cxMailBodyCreator& orig) { }
+
+cxIntegration::cxMailBodyCreator::~cxMailBodyCreator() { }
+
---
+++ cxparser/src/cxmailbodycreator.h
@@ -0,0 +1,26 @@
+/**
+ * @file cxmailbodycreator.h
+ * @brief Declaration file for class cxMailBodyCreator
+ *
+ *  @author		Joao Silva <m.joao.silva.ml at gmail.com>
+ *  @date		Jan 30, 2018
+ */
+
+#ifndef CXMAILBODYCREATOR_H
+#define CXMAILBODYCREATOR_H
+
+namespace cxIntegration
+{
+
+	class cxMailBodyCreator
+	{
+	public:
+		cxMailBodyCreator();
+		cxMailBodyCreator(const cxMailBodyCreator& orig);
+		virtual ~cxMailBodyCreator();
+	private:
+
+	};
+}
+#endif /* CXMAILBODYCREATOR_H */
+
